e1f516c020190b1f366ef9c6f43199d22826f6c8
Mostafa Saeed <Mostafa.Hashhash@lavaloon.com>
2021-03-30 03:02:01 +0200

    getting the mocked response with the location coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{response}}
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return{
+      errorMsg:"",
+      response: ""
+    }
+  },
+
+  methods:{
+    getWeatherInfo(latitude, longitude){
+    // getWeatherInfo(){
+      axios
+      // .get(`http://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/${latitude},${longitude}`
+      // ,{
+      //   headers: {
+      //     'Referrer-Policy': 'no-referrer'
+      //   }
+      // }
+      // )
+
+      let url = process.env.VUE_APP_mockedURL
+      let apiKey = process.env.VUE_APP_API_KEY
+      axios
+      .get(`${url}/${apiKey}/${latitude},${longitude}`)
+      .then(
+        weatherData =>{
+          this.response = weatherData.data
+        }
+      )
+      
+      // console.log(process.env)
+      // console.log(process.env.API_KEY)
+      // console.log(process.env)
+      // console.log(process.env)
+    }
+
+  },
+
+  mounted(){
+
+    if(!("geolocation" in navigator)){
+      this.errorMsg = "Geolocation is not supported"
+      return
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.getWeatherInfo(position.coords.latitude, position.coords.longitude)
+      }
+    )
+    
+
   }
 }
 </script>
@@ -22,7 +72,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+#app{
+  height: 100%;
+  color: white;
+
+}
+body {
+  height: 100%;
+  background: url("./assets/Background.svg") center ;
+  background-repeat: no-repeat;
+  background-size: cover;
+  
+} 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// const instance = axios.create(({
+//   baseURL: `${process.env.baseURL}/${process.env.API_KEY}`
+// }))
 
 new Vue({
   render: h => h(App),
