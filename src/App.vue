d06bca6a24284b098d1562bd21e3f657068da9fc
Mostafa Saeed <Mostafa.Hashhash@lavaloon.com>
2021-03-30 07:55:11 +0200

    correctly displaying the required info

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,53 @@
 <template>
   <div id="app">
-    {{response}}
+
+    <div>
+      <button> F </button>
+      <button> C </button>
+    </div>
+
+      {{ city }} <br>
+      {{ summary }} <br>
+      {{ date }} <br>
+      {{ temperature }} <br>
+      {{ lowTemp }} / {{ highTemp }} <br>
+      {{ todaySummary }}
+
+
+    <div class="daily">
+
+      <ul>
+        <li
+          v-for="day in daily_list.data"
+          :key="day.time"
+        >
+          <p>
+            {{ toDay(day.time) }}
+          </p>
+          <img src="./assets/sunny.svg" alt="">
+          <p>
+            {{ Math.round(day.temperatureHigh) }}
+          </p>
+        </li>
+
+      </ul>
+    </div>
+
+    <div class="hourly">
+
+      <ul>
+        <li
+          v-for="index in 24"
+          :key="index"
+        >
+          <p> {{ toHourly(hourly_list.data[index].time) }} </p>
+          <img src="./assets/sunny.svg" alt="">
+          <p> {{ Math.round(hourly_list.data[index].temperature) }} </p>
+        </li>
+
+      </ul>
+    </div>
+
 
   </div>
 </template>
@@ -14,11 +61,38 @@ export default {
   data(){
     return{
       errorMsg:"",
-      response: ""
+      response: "",
+      daily_list: "",
+      hourly_list: "",
+      city: "",
+      summary: "",
+      date: "",
+      temperature: "",
+      lowTemp: "",
+      highTemp: "",
+      todaySummary: ""
     }
   },
 
   methods:{
+
+    toCelsius(temp){
+      return Math.round((temp*(9/5))+32)
+    },
+
+    toFahrenheit(temp){
+      return Math.round((5/9)*(temp-32))
+    },
+
+    toHourly(time){
+      return new Date(time*1000).toLocaleString('en-us', { minute:"numeric", hour: "numeric" })
+    },
+
+    toDay(time){
+      return new Date(time*1000).toLocaleString('en-us', { weekday: "short"})
+    },
+
+
     getWeatherInfo(latitude, longitude){
     // getWeatherInfo(){
       axios
@@ -36,14 +110,23 @@ export default {
       .get(`${url}/${apiKey}/${latitude},${longitude}`)
       .then(
         weatherData =>{
-          this.response = weatherData.data
+
+          this.city = weatherData.data.timezone.split('/').pop()
+
+          this.summary = weatherData.data.currently.summary
+          this.date = new Date(weatherData.data.currently.time * 1000).toLocaleDateString('en-us',{weekday: "long", day: "numeric", year: "numeric"})
+          this.temperature = weatherData.data.currently.temperature
+          
+          this.lowTemp = weatherData.data.daily.data[0].temperatureLow
+          this.highTemp = weatherData.data.daily.data[0].temperatureHigh
+          this.todaySummary = weatherData.data.daily.data[0].summary
+          
+          this.hourly_list = weatherData.data.hourly
+          this.daily_list = weatherData.data.daily
+
+
         }
       )
-      
-      // console.log(process.env)
-      // console.log(process.env.API_KEY)
-      // console.log(process.env)
-      // console.log(process.env)
     }
 
   },
@@ -82,9 +165,26 @@ export default {
 }
 body {
   height: 100%;
-  background: url("./assets/Background.svg") center ;
+  background: url("./assets/Background.png") center ;
   background-repeat: no-repeat;
   background-size: cover;
   
 } 
+
+ul{
+  list-style: none;
+}
+
+ul li {
+    display: inline-block;
+    margin: 10px;
+
+}
+
+.hourly, .daily {
+  overflow: auto ;
+  white-space: nowrap;
+
+  
+}
 </style>
